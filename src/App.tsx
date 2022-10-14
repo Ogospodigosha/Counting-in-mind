bec14c17d1c673bdbe76622979f8213353fe49f6
4aR1i <kopninn@list.ru>
2022-10-14 19:43:28 +0300

    add value on display

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,17 @@
-import React, {useState} from 'react';
-import {v1} from 'uuid';
+import React from 'react';
 import Calculator from './components/calculator';
 
 import s from './scss/index.module.scss';
-import {Test} from "./components/Test";
-
-export type TaskObjType = {
-    id: string
-    title: string
-}
 
 function App() {
-    const [tasks, setTask] = useState<TaskObjType[]>([
-        {id: v1(), title: "1 + 2 "},
-        {id: v1(), title: "2 * 2 "},
-        {id: v1(), title: "3 + 5 "}
-    ])
-    const [index, setIndex] = useState(0);
-
-    function addElement(tasks:TaskObjType[]) {
-        setIndex(index + 1);
-    }
-
-    function getElements(tasks:TaskObjType[], index:number) {
-        return tasks.slice(0, index).map(el => {
-            return <div key={el.id}>{el.title}</div>;
-        });
-    }
-
-    return (
-        <div className={s.container}>
-            <Calculator tasks={tasks}/>
-            <div className={s.theory}>
-
-                <input type="text" readOnly className={s.theory__area}/>
-                <button onClick={()=>addElement(tasks)}>Add item</button>
-                {getElements(tasks, index)}
-            </div>
-        </div>
-    );
+  return (
+    <div className={s.container}>
+      <Calculator />
+      <div className={s.theory}>
+        <input type="text" readOnly className={s.theory__area} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
